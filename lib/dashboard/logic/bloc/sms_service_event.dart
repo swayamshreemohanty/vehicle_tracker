2fe004eabe6f791d85237b2048c668a8413e2183
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-05-12 20:23:54 +0530

    context param added to fetch sms event

--- lib/dashboard/logic/bloc/sms_service_event.dart
+++ lib/dashboard/logic/bloc/sms_service_event.dart
@@ -5,7 +5,11 @@ abstract class SmsServiceEvent {}
 
 class ListenToSms extends SmsServiceEvent {}
 
-class FetchSms extends SmsServiceEvent {}
+class FetchSms extends SmsServiceEvent {
+  final BuildContext context;
+
+  FetchSms({required this.context});
+}
 
 class SendSms extends SmsServiceEvent {
   final String command;
