f910794782d363168bcfe2c53b6cf195f99a60fa
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-05-12 21:23:37 +0530

    logic updated for sms handling

--- lib/dashboard/logic/bloc/sms_service_bloc.dart
+++ lib/dashboard/logic/bloc/sms_service_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:telephony/telephony.dart';
 import 'package:vehicle_tracker/dashboard/utility/show_snak_bar.dart';
@@ -11,37 +12,91 @@ class SmsServiceBloc extends Bloc<SmsServiceEvent, SmsServiceState> {
     on<SmsServiceEvent>((event, emit) async {
       if (event is SendSms) {
         late SmsSendStatusListener listener;
-        void showRequestStatus(SendStatus status) {
-          switch (status) {
-            case SendStatus.SENT:
-              ShowSnackBar.showSnackBar(event.context, ShowSnackBar.sent);
-              return;
-            case SendStatus.DELIVERED:
-              ShowSnackBar.showSnackBar(event.context, ShowSnackBar.delivered);
-              return;
-            default:
-              ShowSnackBar.showSnackBar(event.context, ShowSnackBar.failed);
-              return;
+        try {
+          void showRequestStatus(SendStatus status) {
+            switch (status) {
+              case SendStatus.SENT:
+                ShowSnackBar.showSnackBar(event.context, ShowSnackBar.sent);
+                return;
+              case SendStatus.DELIVERED:
+                ShowSnackBar.showSnackBar(
+                    event.context, ShowSnackBar.delivered);
+                return;
+              default:
+                ShowSnackBar.showSnackBar(event.context, ShowSnackBar.failed);
+                return;
+            }
           }
-        }
 
-        final permissionsGranted =
-            await smsService.requestPhoneAndSmsPermissions;
-            
-        if (permissionsGranted ?? false) {
-          listener = (SendStatus status) {
-            showRequestStatus(status);
-          };
-          await smsService.sendSms(
-            to: "7609934272",
-            message: "hello",
-            statusListener: listener,
-          );
+          final permissionsGranted =
+              await smsService.requestPhoneAndSmsPermissions;
+
+          if (permissionsGranted ?? false) {
+            listener = (SendStatus status) {
+              showRequestStatus(status);
+            };
+
+            await smsService.sendSms(
+              to: "7609934272",
+              message: "hello",
+              statusListener: listener,
+            );
+          }
+        } catch (e) {
+          if (e is PlatformException) {
+            ShowSnackBar.showSnackBar(
+              event.context,
+              e.message ?? "Error occured.",
+            );
+          } else {
+            ShowSnackBar.showSnackBar(event.context, 'Something went wrong.');
+          }
         }
         return;
       } else if (event is ListenToSms) {
+        smsService.listenIncomingSms(
+          onNewMessage: (SmsMessage message) {
+            print("************New msg received**********");
+            print(message.address);
+            print(message.body);
+          },
+          listenInBackground: true,
+          onBackgroundMessage: backgrounMessageHandler,
+        );
+        return;
       } else if (event is FetchSms) {
-      } else {}
+        try {
+          final permissionsGranted =
+              await smsService.requestPhoneAndSmsPermissions;
+          if (permissionsGranted ?? false) {
+            List<SmsMessage> messages = await smsService.getInboxSms(
+              columns: [SmsColumn.ADDRESS, SmsColumn.BODY],
+              filter:
+                  SmsFilter.where(SmsColumn.ADDRESS).equals("+917609934272"),
+              sortOrder: [
+                OrderBy(SmsColumn.BODY, sort: Sort.DESC),
+                // OrderBy(SmsColumn.BODY)
+              ],
+            );
+            print("Printing list");
+            print(messages.length);
+            for (var element in messages) {
+              print(element.body);
+            }
+          }
+          return;
+        } catch (e) {
+          if (e is PlatformException) {
+            print(e.message);
+            ShowSnackBar.showSnackBar(
+              event.context,
+              e.message ?? "Error occured.",
+            );
+          } else {
+            ShowSnackBar.showSnackBar(event.context, 'Something went wrong.');
+          }
+        }
+      }
     });
   }
   final Telephony smsService = Telephony.instance;
@@ -51,17 +106,5 @@ class SmsServiceBloc extends Bloc<SmsServiceEvent, SmsServiceState> {
     print("************Background msg received**********");
     print(message.address);
     print(message.body);
-  }
-
-  void listenIncomingMessage() {
-    smsService.listenIncomingSms(
-      onNewMessage: (SmsMessage message) {
-        print("************New msg received**********");
-        print(message.address);
-        print(message.body);
-      },
-      listenInBackground: true,
-      onBackgroundMessage: backgrounMessageHandler,
-    );
   }
 }
