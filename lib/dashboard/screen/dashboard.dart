1f76a5b6a159436dac3d127f588a013601d89071
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-05-12 19:53:55 +0530

    incoming sms listener initializer added

--- lib/dashboard/screen/dashboard.dart
+++ lib/dashboard/screen/dashboard.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:vehicle_tracker/dashboard/logic/bloc/sms_service_bloc.dart';
 
 class DashBoardScreen extends StatefulWidget {
   const DashBoardScreen({Key? key}) : super(key: key);
@@ -10,6 +12,12 @@ class DashBoardScreen extends StatefulWidget {
 }
 
 class _DashBoardScreenState extends State<DashBoardScreen> {
+  @override
+  void initState() {
+    context.read<SmsServiceBloc>().listenIncomingMessage();
+    super.initState();
+  }
+
   final _pageTitleTextStyle = GoogleFonts.poppins(
       textStyle: TextStyle(
     fontWeight: FontWeight.w600,
@@ -25,6 +33,15 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
           'Vehicle Tracker',
           style: _pageTitleTextStyle,
         ),
+      ),
+      body: Center(
+        child: OutlinedButton(
+            onPressed: () {
+              context
+                  .read<SmsServiceBloc>()
+                  .add(SendSms(command: 'HIII', context: context));
+            },
+            child: const Text('Send SMS')),
       ),
     );
   }
