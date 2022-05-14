ce9cdec5e004b7eeebef901324ecac7d113b13ef
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-05-14 12:15:29 +0530

    sms decoder added

---
+++ lib/config/sms_decoder.dart
@@ -0,0 +1,14 @@
+import 'package:vehicle_tracker/config/sms_decoder_enum.dart';
+
+class SMSDecoder {
+  static SmsDecoder decodeSMS({required String sms}) {
+    if (sms.contains('your vehicle location')) {
+      return SmsDecoder.location;
+    }
+    if (sms.contains('your vehicle status')) {
+      return SmsDecoder.checkStatus;
+    } else {
+      return SmsDecoder.startStop;
+    }
+  }
+}
