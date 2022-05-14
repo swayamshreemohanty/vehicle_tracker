c7e2621870ccb1a54107e0d080da7837d5c4dbfb
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-05-14 13:51:06 +0530

    status cubit added

--- lib/dashboard/logic/dashboard_data_controller/dashboarddatacontroller_cubit.dart
+++ lib/dashboard/logic/dashboard_data_controller/dashboarddatacontroller_cubit.dart
@@ -1,20 +1,24 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:telephony/telephony.dart';
 import 'package:vehicle_tracker/config/sms_decoder.dart';
 import 'package:vehicle_tracker/config/sms_decoder_enum.dart';
 import 'package:vehicle_tracker/dashboard/logic/update_location/updatelocation_cubit.dart';
+import 'package:vehicle_tracker/dashboard/logic/update_status/update_status_cubit.dart';
 
 part 'dashboarddatacontroller_state.dart';
 
 class DashboarddatacontrollerCubit extends Cubit<DashboarddatacontrollerState> {
   UpdatelocationCubit updatelocationCubit;
+  UpdateStatusCubit updateStatusCubit;
   DashboarddatacontrollerCubit({
     required this.updatelocationCubit,
+    required this.updateStatusCubit,
   }) : super(DashboarddatacontrollerInitial());
 
   Future<void> handleSMSList({required List<SmsMessage> smsList}) async {
     bool locationDatafound = false;
+    bool statusDatafound = false;
     for (var message in smsList) {
       if (SMSDecoder.decodeSMS(sms: message.body ?? 'N/A') ==
               SmsDecoder.location &&
@@ -22,9 +26,18 @@ class DashboarddatacontrollerCubit extends Cubit<DashboarddatacontrollerState> {
         await updatelocationCubit.updateLocation(sms: message.body ?? 'N/A');
         locationDatafound = true;
       }
+      if (SMSDecoder.decodeSMS(sms: message.body ?? 'N/A') ==
+              SmsDecoder.checkStatus &&
+          !statusDatafound) {
+        await updateStatusCubit.updateStaus(sms: message.body ?? 'N/A');
+        statusDatafound = true;
+      }
     }
     if (!locationDatafound) {
       await updatelocationCubit.noLocationFound();
+    }
+    if (!statusDatafound) {
+      await updateStatusCubit.noStatusFound();
     }
     return;
   }
@@ -34,6 +47,9 @@ class DashboarddatacontrollerCubit extends Cubit<DashboarddatacontrollerState> {
     switch (smsType) {
       case SmsDecoder.location:
         await updatelocationCubit.updateLocation(sms: sms);
+        return;
+      case SmsDecoder.checkStatus:
+        await updateStatusCubit.updateStaus(sms: sms);
         return;
       default:
         return;
